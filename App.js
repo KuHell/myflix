4d326d0e1aeb20e6f1aaa9e9022364ad0f567c5f
KuHell <itkdgus489@gmail.com>
2022-10-11 18:15:29 +0900

    add: README, loading확인추가

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import AppLoading from "expo-app-loading";
 import React from "react";
 
+// loading페이지 게속 출력(로딩 로직이 추가 되었는지 확인)
 export default function App() {
-  return null;
+  return <AppLoading />;
 }
