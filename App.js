6d61d00721402cb0e6df27c3f3052a8c946a5d0e
KuHell <itkdgus489@gmail.com>
2022-10-11 18:36:49 +0900

    onFInish, onError 추가

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import AppLoading from "expo-app-loading";
-import React from "react";
+import React, { useState } from "react";
 
-// loading페이지 게속 출력(로딩 로직이 추가 되었는지 확인)
 export default function App() {
-  return <AppLoading />;
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return <AppLoading onFinish={onFinish} onError={console.error} />;
+  }
+  return null;
 }
+
+// export default function App() {
+//   const [ready, setReady] = useState(false);
+//   const onFinish = () => setReady(true);
+//   if (!ready) {
+//     return <AppLoading onFinish={onFinish} onError={console.error} />;
+//   }
+//   return null;
+// }
