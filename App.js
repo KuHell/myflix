9420290dba4faa8cf2ff050e1f636e1637da23f6
KuHell <itkdgus489@gmail.com>
2022-10-29 20:49:20 +0900

    10.29

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./public/img/car.jpg"));
+    await Asset.loadAsync(require("./public/img/car.jpg"));
+    await Image.prefetch;
+  };
   if (!ready) {
-    return <AppLoading onFinish={onFinish} onError={console.error} />;
+    return (
+      <AppLoading
+        onFinish={onFinish}
+        startAsync={startLoading}
+        onError={console.error}
+      />
+    );
   }
   return null;
 }
-
-// export default function App() {
-//   const [ready, setReady] = useState(false);
-//   const onFinish = () => setReady(true);
-//   if (!ready) {
-//     return <AppLoading onFinish={onFinish} onError={console.error} />;
-//   }
-//   return null;
-// }
