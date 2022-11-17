cfe0180454c64ea96a0500013268255fcc4480bc
KuHell <itkdgus489@gmail.com>
2022-11-17 15:05:17 +0900

    add: styled-component

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+// import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+
+const Title = styled.Text``;
+
+const Btn = styled.TouchableOpacity``;
 
 const Movies = ({ navigation: { navigate } }) => {
   return (
-    <TouchableOpacity
+    <Btn
       onPress={() => {
         navigate("Stack", { screen: "One" });
       }}
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <Text>Movies</Text>
-    </TouchableOpacity>
+      <Title>Movies</Title>
+    </Btn>
   );
 };
 
