3f0ef355387b22b0bc37d20a63a1db1831928ad4
KuHell <itkdgus489@gmail.com>
2022-11-17 11:08:57 +0900

    add: Stack style 및 작동

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,12 +1,37 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, useColorScheme } from "react-native";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => {
+  const isTheme = useColorScheme() === "dark";
+
   return (
-    <NativeStack.Navigator>
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false, // 뒤로가기 버튼옆에 전title표시
+        // headerShown: false,                 // header titme 제거
+        headerTintColor: "#ffa801", // title color
+        presentation: "modal", // modal창 처럼 화면전환
+
+        // styles
+        tabBarStyle: { backgroundColor: isTheme ? "#1e272e" : "#fff" },
+        tabBarActiveTintColor: isTheme ? "#ffa801" : "#1e272e",
+        tabBarInactiveTintColor: isTheme ? "#d2dae2" : "#d2dae2",
+        headerStyle: {
+          backgroundColor: isTheme ? "#1e272e" : "#fff",
+        },
+        headerTitleStyle: {
+          color: isTheme ? "#fff" : "#1e272e",
+        },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
       <NativeStack.Screen name="One" component={ScreenOne} />
       <NativeStack.Screen name="Two" component={ScreenTwo} />
       <NativeStack.Screen name="Three" component={ScreenThree} />
@@ -16,20 +41,25 @@ const Stack = () => {
 
 export default Stack;
 
-const ScreenOne = () => (
-  <View>
-    <Text>One</Text>
-  </View>
+const ScreenOne = ({ navigation: { navigate } }) => (
+  <TouchableOpacity onPress={() => navigate("Two")}>
+    <Text>Go Two</Text>
+  </TouchableOpacity>
 );
 
-const ScreenTwo = () => (
-  <View>
-    <Text>Two</Text>
-  </View>
+const ScreenTwo = ({ navigation: { navigate, goBack } }) => (
+  <>
+    <TouchableOpacity onPress={() => navigate("Three")}>
+      <Text>Go Three</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => goBack()}>
+      <Text>Go Back</Text>
+    </TouchableOpacity>
+  </>
 );
 
-const ScreenThree = () => (
-  <View>
-    <Text>three</Text>
-  </View>
+const ScreenThree = ({ navigation: { navigate } }) => (
+  <TouchableOpacity onPress={() => navigate("One")}>
+    <Text>Go One</Text>
+  </TouchableOpacity>
 );
