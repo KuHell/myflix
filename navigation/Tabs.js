3ed12c27cac28041aff17445dfb92332858bc34b
KuHell <itkdgus489@gmail.com>
2022-11-17 10:08:55 +0900

    add: TabBar Style

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,7 +4,7 @@ import Movies from "../screens/Movies";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
 import { View, Text, useColorScheme } from "react-native";
-// import { useColorScheme } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -24,11 +24,61 @@ const Tabs = () => {
         headerTitleStyle: {
           color: isTheme ? "#fff" : "#1e272e",
         },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 12,
+          fontWeight: "600",
+        },
       }}
     >
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 1 }} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            console.log(focused, color, size);
+            return (
+              <Ionicons
+                name={focused ? "film" : "film-outline"}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            console.log(focused, color, size);
+            return (
+              <Ionicons
+                name={focused ? "tv" : "tv-outline"}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            console.log(focused, color, size);
+            return (
+              <Ionicons
+                name={focused ? "search" : "search-outline"}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
